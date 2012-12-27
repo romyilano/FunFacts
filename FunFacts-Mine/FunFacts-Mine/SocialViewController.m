ea05557384c01f278bacdf6caf18e9a19a88d2aa
Romy Ilano <romy@snowyla.com>
2012-12-27 14:36:49 -0800

    added random author + each author has a random quote

--- FunFacts-Mine/FunFacts-Mine/SocialViewController.m
+++ FunFacts-Mine/FunFacts-Mine/SocialViewController.m
@@ -35,12 +35,24 @@
     NSLog(@"Authors Array = %@", self.authorsArray);
 }
 
+// I need to set it as becomeFirstResponder YES
+-(BOOL)canBecomeFirstResponder
+{
+    return YES;
+}
+
+// need to set this becomeFirstResponder before i can shake
+-(void)viewDidAppear:(BOOL)animated
+{
+    [self becomeFirstResponder];
+}
+
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-#pragma mark-Custom Getters
+#pragma mark-Custom Getters for Lazy Loading
 // this is an example of lazy loading for the FunFacts plist. i think this makes the app run faster
 -(NSArray *)authorsArray
 {
@@ -50,6 +62,7 @@
     }
     return _authorsArray;
 }
+
 
 #pragma mark-Actions
 -(IBAction)actionTapped
@@ -61,4 +74,43 @@
 {
     
 }
+
+#pragma mark-Shake
+
+
+
+-(void)motionEnded:(UIEventSubtype)motion withEvent:(UIEvent *)event
+{
+    if(motion==UIEventSubtypeMotionShake) {
+        // 1 - choose a random author
+        NSUInteger authorRandSize = self.authorsArray.count;
+        NSUInteger authorRandomIndex =  (arc4random() % ((unsigned) authorRandSize));
+        
+        // 2 - using random index for an author int he array, the dictionary corresponding to the author is retrieved from the self.authorsArray property and stored in an NSDictionary called authorDictionary
+        NSDictionary *authorDictionary = self.authorsArray[authorRandomIndex];
+        
+        // 3 - Using the #defines you set up earlier, pulls out information
+        //  about the author from the dictionary.
+        // Note that this is new literal syntax
+        NSArray *facts = authorDictionary[AuthorFactsKey];
+        NSString *image = authorDictionary[AuthorImageKey];
+        NSString *name = authorDictionary[AuthorNameKey];
+        NSString *twitter = authorDictionary[AuthorTwitterKey];
+        
+        // 4 - another random index has to be called to pull a random fact about the current author
+        NSUInteger factsRandSize = facts.count;
+        NSUInteger factsRandomIndex = (arc4random() % ((unsigned) factsRandSize));
+        
+        // 5 - title label is unhidden
+        //      appropriate UI elements are populated with the randomly acquired author data
+        self.factTitleLabel.hidden=NO;
+        self.factTextView.text = facts[factsRandomIndex];
+        self.nameLabel.text =  name;
+        self.twitterLabel.text = twitter;
+        self.authorImageView.image = [UIImage imageNamed:image];
+        
+    }
+}
+
+
 @end
--- README.md
+++ README.md
@@ -6,9 +6,13 @@ Social Frameworks tutorial from RayWenderlich.com - with many learnings. Going t
 
 # Notes
 
+## Shaking (from UIResponder)
+In order for the view controller to detect motions + gestures, it needs to tell teh app that it can become the first responder, and then actually become the first responder when it enters the foreground.
+
 ## Lazy Loading (p 766)
 A good practice is to "lazy load" whatever you need into memroy. That just means not creating instances of objects or loading items into memory unti they are required. (not an ironclad law). Creating a custom getter for authorsArray (which links to the plist)
 
 ## Use Define Statements for Strings
 
 Use #define statements for strings when accessing contents of a dictionary by key. The advantage of using a defined key is that you can easily change it once, and immediately have it updated wherever it's used.
+
